c31d4aa998983f6587ca2deab8fc744bcfa0e489
yerassyl-saiman <yerassyl.s@1fit.app>
2025-04-18 02:44:24 +0500

    [feat] add basic setup

---
+++ Pinterest/Pinterest/PinterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestApp.swift
+//  Pinterest
+//
+//  Created by Yerassyl Saiman on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct PinterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
