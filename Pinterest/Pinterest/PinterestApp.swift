c8b6941dba5c61c7c0bac5e669fc2dcce266d151
yerassyl-saiman <yerassyl.s@1fit.app>
2025-04-18 03:01:15 +0500

    [refactor] refactor code

--- Pinterest/Pinterest/PinterestApp.swift
+++ Pinterest/Pinterest/PinterestApp.swift
@@ -1,17 +1,10 @@
-//
-//  PinterestApp.swift
-//  Pinterest
-//
-//  Created by Yerassyl Saiman on 18.04.2025.
-//
-
 import SwiftUI
 
 @main
 struct PinterestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ImageGalleryView()
         }
     }
 }
